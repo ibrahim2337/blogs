cb5d13922ef3e90ab294aeeaaa911ed08772be0a
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-07-12 18:49:04 +0600

    footer complate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./component/Footer/Footer";
 import Navbar from "./component/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
